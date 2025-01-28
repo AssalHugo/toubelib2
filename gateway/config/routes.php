26faca9510514a8a09f9d5c9c0ce22c7b131a46e
hugo assal <hugo.assal1@etu.univ-lorraine.fr>
2025-01-28 14:16:12 +0100

    épuration code

--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -7,9 +7,7 @@ use Gateway\Actions\GenericGetCatalogAction;
 use Gateway\Actions\ListePraticiensAction;
 use Gateway\Actions\ListerDispoPraticienAction;
 use Gateway\Actions\PraticienAction;
-use Gateway\Actions\SigninAction;
 use Slim\App;
-use Gateway\middlewares\AddHeaders;
 
 
 return function (App $app): App {
@@ -19,14 +17,6 @@ return function (App $app): App {
     $app->get('/praticiens', ListePraticiensAction::class);
 
     $app->get('/praticiens/{id}',  PraticienAction::class);
-
-    $app->get('/praticiens/{id}/planning', GenericGetCatalogAction::class)
-        ->add(new AddHeaders);
-
-    $app->post('/auth/signin', SigninAction::class)
-        ->add(new AddHeaders);
-
-    $app->get('/praticiens/{id}/planning', GenericGetCatalogAction::class);
 
     $app->post('/rdvs', CreerRendezVousAction::class);
 
